87db6223b246bb7826d4773e99102344dec67b52
Jozef Fraai <jozeffraai@gmail.com>
2015-03-10 10:21:56 +0100

    KLIK NIET OP MAPS!!
    maps geimplementeerd, zit op dit moment nog een bug in maar dat los ik
    zo op

---
+++ WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/MapsActivity.java
@@ -0,0 +1,13 @@
+package com.testapplication.wfcmainpage;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MapsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_maps);
+	}
+}
