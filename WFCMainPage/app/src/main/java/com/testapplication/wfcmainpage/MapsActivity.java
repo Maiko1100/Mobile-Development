2475a0c02f3c5db8d3b36681e71ac1742e7a3a5b
Jozef Fraai <jozeffraai@gmail.com>
2015-03-30 10:47:04 +0200

    Code cleanup and added Comments

--- WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/MapsActivity.java
+++ WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/MapsActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -18,114 +17,107 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends ActionBarActivity implements View.OnClickListener {
-    private final LatLng LOCATION_WFC = new LatLng(52.354484, 4.841304);
-    private final String LOCATION_WFC_STRING = "Koningin Wilhelminaplein 13 1062 HH Amsterdam";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        setTitle(getString(R.string.maps_title_text));
+	private final LatLng LOCATION_WFC = new LatLng(52.354484, 4.841304);
+	private final String LOCATION_WFC_STRING = "Koningin Wilhelminaplein 13 1062 HH Amsterdam";
 
-        ImageButton buttonCar = (ImageButton) findViewById(R.id.buttonCar);
-        ImageButton buttonWalk = (ImageButton) findViewById(R.id.buttonWalk);
-        ImageButton buttonBicycle = (ImageButton) findViewById(R.id.buttonBicycle);
-        ImageButton buttonOV = (ImageButton) findViewById(R.id.buttonOV);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_maps);
+		setTitle(getString(R.string.maps_title_text));
 
-        buttonCar.setOnClickListener(this);
-        buttonWalk.setOnClickListener(this);
-        buttonBicycle.setOnClickListener(this);
-        buttonOV.setOnClickListener(this);
+		ImageButton buttonCar = (ImageButton) findViewById(R.id.buttonCar);
+		ImageButton buttonWalk = (ImageButton) findViewById(R.id.buttonWalk);
+		ImageButton buttonBicycle = (ImageButton) findViewById(R.id.buttonBicycle);
+		ImageButton buttonOV = (ImageButton) findViewById(R.id.buttonOV);
 
-        GoogleMap mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+		buttonCar.setOnClickListener(this);
+		buttonWalk.setOnClickListener(this);
+		buttonBicycle.setOnClickListener(this);
+		buttonOV.setOnClickListener(this);
 
-        CameraUpdate start = CameraUpdateFactory.newLatLngZoom(LOCATION_WFC, 18);
+		GoogleMap mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+		mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 
-        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-            @Override
-            public View getInfoWindow(Marker marker) {
-                return null;
-            }
+		// Zoom on location on start
+		CameraUpdate start = CameraUpdateFactory.newLatLngZoom(LOCATION_WFC, 18);
+		mMap.animateCamera(start);
 
-            @Override
-            public View getInfoContents(Marker pMarker) {
-                // Getting view from the layout file info_window_layout
-                View v = getLayoutInflater().inflate(R.layout.info_window_layout, null);
+		// Set custom marker text
+		mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+			@Override
+			public View getInfoWindow(Marker marker) {
+				return null;
+			}
 
-                // Getting the position from the marker
-                LatLng latLng = pMarker.getPosition();
+			@Override
+			public View getInfoContents(Marker pMarker) {
+				// Getting view from the layout file info_window_layout
+				View v = getLayoutInflater().inflate(R.layout.info_window_layout, null);
 
-                // Getting reference to the TextView to set info text
-                TextView infoText = (TextView) v.findViewById(R.id.tv_info_window_text);
+				return v;
+			}
+		});
 
-                // Setting the info text
-                //infoText.setText());
+		// Show custom marker
+		MarkerOptions mMarkerOptions = new MarkerOptions();
+		mMarkerOptions.position(LOCATION_WFC);
+		Marker mMarker = mMap.addMarker(mMarkerOptions);
+		mMarker.showInfoWindow();
+		mMarker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.kleinlogo));
+	}
 
-                return v;
-            }
-        });
+	//Start navigation
+	public void startCar(View v) {
+		//Start Google Maps Drive, Navigate immediately
+		Uri gmmIntentUri = Uri.parse("google.navigation:q=" + LOCATION_WFC_STRING + "&mode=d");
+		Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+		mapIntent.setPackage("com.google.android.apps.maps");
+		startActivity(mapIntent);
+	}
 
-        MarkerOptions tempMarkerOptions = new MarkerOptions();
-        tempMarkerOptions.position(LOCATION_WFC);
-        Marker tempMarker = mMap.addMarker(tempMarkerOptions);
-        tempMarker.showInfoWindow();
-        tempMarker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.kleinlogo));
+	public void startWalk(View v) {
+		//Start Google Maps Walk, Navigate immediately
+		Uri gmmIntentUri = Uri.parse("google.navigation:q=" + LOCATION_WFC_STRING + "&mode=w");
+		Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+		mapIntent.setPackage("com.google.android.apps.maps");
+		startActivity(mapIntent);
+	}
 
-        mMap.animateCamera(start);
-        mMap.moveCamera(start);
-        mMap.setMyLocationEnabled(true);
-    }
+	public void startBicycle(View v) {
+		//Start Google Maps Bike, Navigate immediately
+		Uri gmmIntentUri = Uri.parse("google.navigation:q=" + LOCATION_WFC_STRING + "&mode=b");
+		Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+		mapIntent.setPackage("com.google.android.apps.maps");
+		startActivity(mapIntent);
+	}
 
-    public void startCar(View v) {
-        //Start Google Maps Drive, Navigate immediately
-        Uri gmmIntentUri = Uri.parse("google.navigation:q=" + LOCATION_WFC_STRING);
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
-        startActivity(mapIntent);
-    }
+	public void startOV(View v) {
+		//Start Google Maps Public Transport, Navigate immediately
+		Uri gmmIntentUri = Uri.parse("google.navigation:q=" + LOCATION_WFC_STRING + "&mode=r");
+		Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+		mapIntent.setPackage("com.google.android.apps.maps");
+		startActivity(mapIntent);
+	}
 
-    public void startWalk(View v) {
-        //Start Google Maps Walk, Navigate immediately
-        Uri gmmIntentUri = Uri.parse("google.navigation:q=" + LOCATION_WFC_STRING + "&mode=w");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
-        startActivity(mapIntent);
-    }
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
 
-    public void startBicycle(View v) {
-        //Start Google Maps Bike, Navigate immediately
-        Uri gmmIntentUri = Uri.parse("google.navigation:q=" + LOCATION_WFC_STRING + "&mode=b");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
-        startActivity(mapIntent);
-    }
-
-    public void startOV(View v) {
-        //Start Google Maps Public Transport, Navigate immediately
-        Uri gmmIntentUri = Uri.parse("google.navigation:q=" + LOCATION_WFC_STRING + "&mode=r");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-        mapIntent.setPackage("com.google.android.apps.maps");
-        startActivity(mapIntent);
-    }
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-
-        switch (id) {
-            case R.id.buttonCar:
-                startCar(v);
-                break;
-            case R.id.buttonWalk:
-                startWalk(v);
-                break;
-            case R.id.buttonBicycle:
-                startBicycle(v);
-                break;
-            case R.id.buttonOV:
-                startOV(v);
-                break;
-        }
-    }
+		switch (id) {
+			case R.id.buttonCar:
+				startCar(v);
+				break;
+			case R.id.buttonWalk:
+				startWalk(v);
+				break;
+			case R.id.buttonBicycle:
+				startBicycle(v);
+				break;
+			case R.id.buttonOV:
+				startOV(v);
+				break;
+		}
+	}
 }
