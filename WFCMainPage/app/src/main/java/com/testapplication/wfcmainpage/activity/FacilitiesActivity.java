6431fb50b3a2844914bc3aa3d7a19d5dc6420c43
Maiko1100 <maiko1100@gmail.com>
2015-04-29 14:32:54 +0200

    System out gedelete

--- WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/activity/FacilitiesActivity.java
+++ WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/activity/FacilitiesActivity.java
@@ -232,7 +232,6 @@ public class FacilitiesActivity extends ActionBarActivity {
      */
     private void selectDrawerItem(int position) {
         if (position == 0) {
-            System.out.println(position);
             mFacilities = getAllFacilities();
             facilityAdapter = new CustomAdapter(getBaseContext(), mFacilities);
             mTitle.setText(R.string.facilities_button_text);
@@ -240,7 +239,6 @@ public class FacilitiesActivity extends ActionBarActivity {
             mDrawerLayout.closeDrawer(mDrawerList);
             mFacilityList.setAdapter(facilityAdapter);
         } else {
-            System.out.println(position);
             mFacilities = getModeFacilities(position);
             facilityAdapter = new CustomAdapter(getBaseContext(), mFacilities);
             mFacilityList.setAdapter(facilityAdapter);
