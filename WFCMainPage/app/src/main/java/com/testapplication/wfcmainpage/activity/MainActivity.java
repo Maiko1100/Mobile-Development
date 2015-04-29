621edf78d9827624017336f8d795c7ad4eea9eb2
Fren de Haan <fren.de.haan@hva.nl>
2015-04-29 13:32:55 +0200

    transition zoek knopje

--- WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/activity/MainActivity.java
+++ WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/activity/MainActivity.java
@@ -88,28 +88,6 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 		mRentButton.setOnClickListener(this);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_info, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
-
 	/*
 	Method for creating dots in the right amount below the viewpager
 	 */
