c4282475ba5ddcad6b5967cb82b50009b6bd82dc
Jozef Fraai <jozeffraai@gmail.com>
2015-05-13 10:01:25 +0200

    half af

--- WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/activity/NavigationIndoorMapActivity.java
+++ WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/activity/NavigationIndoorMapActivity.java
@@ -5,36 +5,34 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.ImageButton;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.TileOverlayOptions;
-import com.google.android.gms.maps.model.VisibleRegion;
+import com.indooratlas.android.IndoorAtlas;
 import com.testapplication.wfcmainpage.R;
-
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
 
 public class NavigationIndoorMapActivity extends ActionBarActivity implements View.OnClickListener {
 
+	String mApiKey = "0ffdc6fa-5af1-4b36-8898-f132b9b27ea1";
+	String mApiSecret = "8x!XaLPUCDx4DSNlKi6&flJPHpXLKmX&)7IMFn(yy2TcrQRzsuQZvVT%aJwKWop7pwqGN)Bsvlm8zyf%db(dOZZhc82lI22nxM8XVJ7eY5GFw)GZp4sEBEeyGmIyhO6R";
+
+	private IndoorAtlas mIndoorAtlas;
+
+	String mVenueId = "b2f56148-7faa-4ce4-b543-32c67c01b015";
+	String mFloorId = "ce808d5e-6410-4c1e-9796-66b115673eeb";
+	String mFloorPlanId = "dc895a7c-fbb7-43d4-b6e2-7552ad6abab0";
+
 	private final LatLngBounds mMobiBounds = new LatLngBounds(new LatLng(52.3713710997303, 4.88609969615936), new LatLng(52.371600, 4.886524));
 	private final String LOCATION_MOBI_STRING = "Herengracht 266 1016 BV Amsterdam";
-	private final int MAX_ZOOM = 18;
-	private final int MIN_ZOOM = 14;
-	private final GoogleMap mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-	private OverscrollHandler mOverscrollHandler = new OverscrollHandler();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +55,7 @@ public class NavigationIndoorMapActivity extends ActionBarActivity implements Vi
 		buttonOV.setOnClickListener(this);
 
 
+		GoogleMap mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 		mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
 
 		// Zoom on location on start
@@ -79,83 +78,15 @@ public class NavigationIndoorMapActivity extends ActionBarActivity implements Vi
 		});
 
 		// Show custom marker
-		GroundOverlayOptions mobiMap = new GroundOverlayOptions().image(BitmapDescriptorFactory.fromResource(R.drawable.plattegrond)).positionFromBounds(mMobiBounds);
-		mMap.addGroundOverlay(mobiMap);
+		GroundOverlayOptions mMobiMap = new GroundOverlayOptions().image(BitmapDescriptorFactory.fromResource(R.drawable.plattegrond)).positionFromBounds(mMobiBounds);
+		mMap.addGroundOverlay(mMobiMap);
+
+//		mIndoorAtlas = IndoorAtlasFactory.createIndoorAtlas(getApplicationContext(),this,mApiKey,mApiSecret);
+
+
+		mIndoorAtlas.startPositioning(mVenueId, mFloorId, mFloorPlanId);
 	}
 
-	/**
-	 * Returns the correction for Lat and Lng if camera is trying to get outside of visible map
-	 * @param cameraBounds Current camera bounds
-	 * @return Latitude and Longitude corrections to get back into bounds.
-	 */
-	private LatLng getLatLngCorrection(LatLngBounds cameraBounds) {
-		double latitude=0, longitude=0;
-		if(cameraBounds.southwest.latitude < mMobiBounds.southwest.latitude) {
-			latitude = mMobiBounds.southwest.latitude - cameraBounds.southwest.latitude;
-		}
-		if(cameraBounds.southwest.longitude < mMobiBounds.southwest.longitude) {
-			longitude = mMobiBounds.southwest.longitude - cameraBounds.southwest.longitude;
-		}
-		if(cameraBounds.northeast.latitude > mMobiBounds.northeast.latitude) {
-			latitude = mMobiBounds.northeast.latitude - cameraBounds.northeast.latitude;
-		}
-		if(cameraBounds.northeast.longitude > mMobiBounds.northeast.longitude) {
-			longitude = mMobiBounds.northeast.longitude - cameraBounds.northeast.longitude;
-		}
-		return new LatLng(latitude, longitude);
-	}
-
-	/**
-	 * Bounds the user to the overlay.
-	 */
-	private class OverscrollHandler extends Handler {
-		@Override
-		public void handleMessage(Message msg) {
-			CameraPosition position = mMap.getCameraPosition();
-			VisibleRegion region = mMap.getProjection().getVisibleRegion();
-			float zoom = 0;
-			if(position.zoom < MIN_ZOOM) zoom = MIN_ZOOM;
-			if(position.zoom > MAX_ZOOM) zoom = MAX_ZOOM;
-			LatLng correction = getLatLngCorrection(region.latLngBounds);
-			if(zoom != 0 || correction.latitude != 0 || correction.longitude != 0) {
-				zoom = (zoom==0)?position.zoom:zoom;
-				double lat = position.target.latitude + correction.latitude;
-				double lon = position.target.longitude + correction.longitude;
-				CameraPosition newPosition = new CameraPosition(new LatLng(lat,lon), zoom, position.tilt, position.bearing);
-				CameraUpdate update = CameraUpdateFactory.newCameraPosition(newPosition);
-				mMap.moveCamera(update);
-			}
-        /* Recursively call handler every 100ms */
-			sendEmptyMessageDelayed(0,100);
-		}
-
-		@Override
-		public void close() {
-
-		}
-
-		@Override
-		public void flush() {
-
-		}
-
-		@Override
-		public void publish(LogRecord record) {
-
-		}
-	}
-
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-		mContext = getActivity();
-		mMap = getMap();
-		mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
-		mMap.addTileOverlay(new TileOverlayOptions().tileProvider(new VexLocalTileProvider(getResources().getAssets())));
-		CameraUpdate upd = CameraUpdateFactory.newLatLngZoom(new LatLng(41.87145, 12.52849), 14);
-		mMap.moveCamera(upd);
-		mOverscrollHandler.sendEmptyMessageDelayed(0,100);
-	}
 
 	//Start navigation
 	public void startCar(View v) {
