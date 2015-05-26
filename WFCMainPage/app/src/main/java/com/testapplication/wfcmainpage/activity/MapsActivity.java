ffe6589dcda7185b75d832299fcc185b3eb66558
Fren de Haan <fren.de.haan@hva.nl>
2015-05-26 13:48:05 +0200

    Removed unused imports and formatted code

--- WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/activity/MapsActivity.java
+++ WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/activity/MapsActivity.java
@@ -1,19 +1,13 @@
 package com.testapplication.wfcmainpage.activity;
 
 import android.content.Intent;
-import android.content.SyncStatusObserver;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -24,8 +18,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.testapplication.wfcmainpage.R;
-
-import org.apache.http.io.SessionOutputBuffer;
 
 
 public class MapsActivity extends ActionBarActivity implements View.OnClickListener {
@@ -38,8 +30,6 @@ public class MapsActivity extends ActionBarActivity implements View.OnClickListe
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_maps);
 		setTitle(getString(R.string.maps_title_text));
-
-
 
 
 		ImageButton buttonCar = (ImageButton) findViewById(R.id.buttonCar);
@@ -58,8 +48,6 @@ public class MapsActivity extends ActionBarActivity implements View.OnClickListe
 
 		GoogleMap mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 		mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-
-
 
 
 		// Zoom on location on start
@@ -107,7 +95,6 @@ public class MapsActivity extends ActionBarActivity implements View.OnClickListe
 		});
 
 	}
-
 
 
 	//Start navigation
