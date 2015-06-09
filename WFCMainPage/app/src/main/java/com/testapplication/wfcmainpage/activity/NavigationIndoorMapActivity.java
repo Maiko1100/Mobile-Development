ea8cd6f42e6d90c36005259967d9ba3a3503e81a
Jozef Fraai <jozeffraai@gmail.com>
2015-06-09 10:47:17 +0200

    Added javadoc and comments to indoor navigation

--- WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/activity/NavigationIndoorMapActivity.java
+++ WFCMainPage/app/src/main/java/com/testapplication/wfcmainpage/activity/NavigationIndoorMapActivity.java
@@ -1,20 +1,14 @@
 package com.testapplication.wfcmainpage.activity;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.text.style.ScaleXSpan;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.Display;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.android.gms.fitness.data.Device;
 import com.indooratlas.android.CalibrationState;
 import com.indooratlas.android.FloorPlan;
 import com.indooratlas.android.FutureResult;
@@ -29,9 +23,14 @@ import com.testapplication.wfcmainpage.R;
 
 import java.io.IOException;
 
-
+/**
+ * Activity that handles indoor navigation
+ *
+ * @author Team 12
+ */
 public class NavigationIndoorMapActivity extends ActionBarActivity implements IndoorAtlasListener {
 
+	/* Keys necessary to load Floorplan and indoor positioning details */
 	String mApiKey = "0ffdc6fa-5af1-4b36-8898-f132b9b27ea1";
 	String mApiSecret = "8x!XaLPUCDx4DSNlKi6&flJPHpXLKmX&)7IMFn(yy2TcrQRzsuQZvVT%aJwKWop7pwqGN)Bsvlm8zyf%db(dOZZhc82lI22nxM8XVJ7eY5GFw)GZp4sEBEeyGmIyhO6R";
 	String mFloorPlanId = "f1b92acd-7a57-4bdd-8395-6f9a4cb84724";
@@ -42,10 +41,10 @@ public class NavigationIndoorMapActivity extends ActionBarActivity implements In
 	private boolean mIsPositioning;
 	private FloorPlan mFloorPlan;
 
-	ImageView image, blueDot;
+	ImageView ivFloorPLan, blueDot;
 
-	private final String TAG = "MijnTag";
-	double B, fpx, fpy;
+	private final String LOG_TAG = "MIJN_TAG";
+	double mConversion;
 
 
 	@Override
@@ -54,7 +53,7 @@ public class NavigationIndoorMapActivity extends ActionBarActivity implements In
 		setContentView(R.layout.activity_navigation_indoor_map);
 		setTitle(getString(R.string.indoor_maps_title_text));
 
-		image = (ImageView) findViewById(R.id.showIndoorMap);
+		ivFloorPLan = (ImageView) findViewById(R.id.showIndoorMap);
 		blueDot = (ImageView) findViewById(R.id.showBlueDot);
 
 
@@ -66,13 +65,8 @@ public class NavigationIndoorMapActivity extends ActionBarActivity implements In
 			@Override
 			public void onResult(final FloorPlan result) {
 				mFloorPlan = result;
-				B = mFloorPlan.metricToPixelConversion;
-				fpx = mFloorPlan.dimensions[0];
-				fpy = mFloorPlan.dimensions[1];
+				mConversion = mFloorPlan.metricToPixelConversion;
 				loadFloorPlanImage(result);
-
-				Log.e(TAG, "Metric to pixel: " + B + " Normal X: " + fpx + " Scaled X: " + (fpx / B) + " Normal Y: ." + fpy + " Scaled Y: " + (fpy / B));
-
 			}
 
 			@Override
@@ -119,7 +113,7 @@ public class NavigationIndoorMapActivity extends ActionBarActivity implements In
 
 	private void startPositioning() {
 		if (mIndoorAtlas != null) {
-			Log.e(TAG, String.format("startPositioning, venueId: %s, floorId: %s, floorPlanId: %s",
+			Log.e(LOG_TAG, String.format("startPositioning, venueId: %s, floorId: %s, floorPlanId: %s",
 					mVenueId,
 					mFloorId,
 					mFloorPlanId));
@@ -127,43 +121,19 @@ public class NavigationIndoorMapActivity extends ActionBarActivity implements In
 				mIndoorAtlas.startPositioning(mVenueId, mFloorId, mFloorPlanId);
 				mIsPositioning = true;
 			} catch (IndoorAtlasException e) {
-				Log.e(TAG, "startPositioning failed: " + e);
+				Log.e(LOG_TAG, "startPositioning failed: " + e);
 			}
 		} else {
-			Log.e(TAG, "calibration not ready, cannot start positioning");
+			Log.e(LOG_TAG, "calibration not ready, cannot start positioning");
 		}
 	}
 
 	private void stopPositioning() {
 		mIsPositioning = false;
 		if (mIndoorAtlas != null) {
-			Log.e(TAG, "Stop positioning");
+			Log.e(LOG_TAG, "Stop positioning");
 			mIndoorAtlas.stopPositioning();
 		}
-	}
-
-	private BitmapFactory.Options createBitmapOptions(FloorPlan floorPlan) {
-
-		BitmapFactory.Options options = new BitmapFactory.Options();
-
-//		int reqWidth = 2048;
-//		int reqHeight = 2048;
-//		final int width = (int) floorPlan.dimensions[0];
-//		final int height = (int) floorPlan.dimensions[1];
-//		int inSampleSize = 1;
-//
-//		if (height > reqHeight || width > reqWidth) {
-//			final int halfHeight = height / 2;
-//			final int halfWidth = width / 2;
-//			while ((halfHeight / inSampleSize) > reqHeight
-//					&& (halfWidth / inSampleSize) > reqWidth) {
-//				inSampleSize *= 2;
-//			}
-//		}
-//
-//
-//		options.inSampleSize = inSampleSize;
-		return options;
 	}
 
 	void loadFloorPlanImage(FloorPlan floorPlan) {
@@ -175,7 +145,7 @@ public class NavigationIndoorMapActivity extends ActionBarActivity implements In
 				runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-						image.setImageBitmap(result);
+						ivFloorPLan.setImageBitmap(result);
 					}
 				});
 
@@ -194,6 +164,36 @@ public class NavigationIndoorMapActivity extends ActionBarActivity implements In
 		});
 	}
 
+	/**
+	 * Scale down the floorplan image if the size is too big
+	 * @param floorPlan
+	 * @return
+	 */
+
+	private BitmapFactory.Options createBitmapOptions(FloorPlan floorPlan) {
+
+		BitmapFactory.Options options = new BitmapFactory.Options();
+
+		int reqWidth = 2048;
+		int reqHeight = 2048;
+		final int width = (int) floorPlan.dimensions[0];
+		final int height = (int) floorPlan.dimensions[1];
+		int inSampleSize = 1;
+
+		if (height > reqHeight || width > reqWidth) {
+			final int halfHeight = height / 2;
+			final int halfWidth = width / 2;
+			while ((halfHeight / inSampleSize) > reqHeight
+					&& (halfWidth / inSampleSize) > reqWidth) {
+				inSampleSize *= 2;
+			}
+		}
+
+
+		options.inSampleSize = inSampleSize;
+		return options;
+	}
+
 	private void tearDown() {
 		if (mIndoorAtlas != null) {
 			mIndoorAtlas.tearDown();
@@ -206,72 +206,51 @@ public class NavigationIndoorMapActivity extends ActionBarActivity implements In
 	 * This is where you will handle location updates.
 	 */
 	public void onServiceUpdate(ServiceState state) {
-		setImagePoint(state.getMetricPoint());
-
+		setMetricPoint(state.getMetricPoint());
 	}
 
-	private double getMetricCalculatedScale(Double metr) {
-		metr = (metr * B);
+	/**
+	 * Method that scales metric points to floorplan image size and displays the blue dot
+	 * @param mMetricPoint
+	 */
+	private void setMetricPoint(final MetricPoint mMetricPoint) {
 
-//		float res = convertPixelsToDp(metr.floatValue(), this);
-//
-//		metr = (double) res;
-		return metr;
-	}
-
-
-	public static float convertPixelsToDp(float px, Context context) {
-		Resources resources = context.getResources();
-		DisplayMetrics metrics = resources.getDisplayMetrics();
-		float dp = px / (metrics.densityDpi / 160f);
-		return dp;
-	}
-
-	private void setImagePoint(final MetricPoint imgPt) {
-
-//		double dx = getMetricCalculatedScale(imgPt.getX());
-//		double dy = getMetricCalculatedScale(imgPt.getY());
-		float x = (float) getMetricCalculatedScale(imgPt.getX());
-		float y = (float) getMetricCalculatedScale(imgPt.getY());
-
+		float x = (float) (mMetricPoint.getX() * mConversion);
+		float y = (float) (mMetricPoint.getY() * mConversion);
 
 		blueDot.setX(x);
 		blueDot.setY(y);
-
-
-		Log.e(TAG, "Print" + "\nFloat Output Y: " + y + " Float Output X: " + " image X: " + image.getX());
-
 	}
 
 
 	@Override
 	public void onServiceFailure(int errorCode, String reason) {
-		Log.e(TAG, "onServiceFailure: reason : " + reason);
+		Log.e(LOG_TAG, "onServiceFailure: reason : " + reason);
 	}
 
 	@Override
 	public void onServiceInitializing() {
-		Log.e(TAG, "onServiceInitializing");
+		Log.e(LOG_TAG, "onServiceInitializing");
 	}
 
 	@Override
 	public void onServiceInitialized() {
-		Log.e(TAG, "onServiceInitialized");
+		Log.e(LOG_TAG, "onServiceInitialized");
 	}
 
 	@Override
 	public void onInitializationFailed(final String reason) {
-		Log.e(TAG, "onInitializationFailed: " + reason);
+		Log.e(LOG_TAG, "onInitializationFailed: " + reason);
 	}
 
 	@Override
 	public void onServiceStopped() {
-		Log.e(TAG, "onServiceStopped");
+		Log.e(LOG_TAG, "onServiceStopped");
 	}
 
 	@Override
 	public void onCalibrationStatus(CalibrationState calibrationState) {
-		Log.e(TAG, "onCalibrationStatus, percentage: " + calibrationState.getPercentage());
+		Log.e(LOG_TAG, "onCalibrationStatus, percentage: " + calibrationState.getPercentage());
 	}
 
 	/**
@@ -280,7 +259,7 @@ public class NavigationIndoorMapActivity extends ActionBarActivity implements In
 	 */
 	@Override
 	public void onCalibrationReady() {
-		Log.e(TAG, "onCalibrationReady");
+		Log.e(LOG_TAG, "onCalibrationReady");
 		Context context = getApplicationContext();
 		CharSequence text = "Calibration is ready";
 		int duration = Toast.LENGTH_SHORT;
